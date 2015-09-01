73573e85d780fc69c97bd3db2c44cb8644b9d193
Pedro Monteiro <pedro.monteiro@telecom.pt>
2015-09-01 12:50:27 +0100

    work in progress

---
+++ ATA32_LGCIU.cpp
@@ -0,0 +1,12 @@
+#include "ATA32_LGCIU.h"
+
+
+
+ATA32_LGCIU::ATA32_LGCIU()
+{
+}
+
+
+ATA32_LGCIU::~ATA32_LGCIU()
+{
+}
---
+++ ATA32_LGCIU.h
@@ -0,0 +1,8 @@
+#pragma once
+class ATA32_LGCIU
+{
+public:
+	ATA32_LGCIU();
+	~ATA32_LGCIU();
+};
+
