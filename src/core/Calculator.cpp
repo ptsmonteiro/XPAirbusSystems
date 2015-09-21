87b9e96847bf0f5d6f2461666d57238b0dd01042
Tiago Matias <tiago.matias@gmail.com>
2015-09-22 00:00:01 +0100

    adiru work

---
+++ src/core/Calculator.cpp
@@ -0,0 +1,10 @@
+#include <math.h>
+#include "core\core.h"
+
+
+float Calculator::pressureAltitudeFt(float pressureInHg, float qnhInHg) {
+	// http://williams.best.vwh.net/avform.htm#Altimetry
+	// alt_set in inches, heights in feet => P_alt_corr = 145442.2*(1 - (alt_set / 29.92126) ^ 0.190261)
+	
+	return 145442.2*(1 - pow((pressureInHg / qnhInHg), 0.190261));
+}
